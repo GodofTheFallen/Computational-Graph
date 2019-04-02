fa59906b0d1606dbd713fffe3fddbc8d783fc6d2
GodofTheFallen <gtf_2000@qq.com>
2019-04-03 00:09:31 +0800

    增加了基类node，Task增加了对文件名的要求

---
+++ Raw/node.h
@@ -0,0 +1,14 @@
+#ifndef COMPUTATIONAL_GRAPH_NODE_H
+#define COMPUTATIONAL_GRAPH_NODE_H
+
+template<typename _T>
+class Node
+{
+private:
+public:
+    virtual _T GetVal() = 0; //获取节点的计算结果
+    virtual _T Clear() = 0;  //清除节点的计算结果
+};
+
+
+#endif //COMPUTATIONAL_GRAPH_NODE_H
