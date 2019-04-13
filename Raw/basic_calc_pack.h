bb6ffb5e51de6981a11a25efc490115326447983
GodofTheFallen <gtf_2000@qq.com>
2019-04-13 11:32:05 +0800

    更新比较节点的命名规范，在task.md

--- Raw/basic_calc_pack.h
+++ Raw/basic_calc_pack.h
@@ -1,4 +1,8 @@
+#ifndef COMPUTATIONAL_GRAPH_BASIC_CALC_PACK_H
+#define COMPUTATIONAL_GRAPH_BASIC_CALC_PACK_H
 #include "plucnode.h"
 #include "mincnode.h"
 #include "mulcnode.h"
 #include "divcnode.h"
+
+#endif //COMPUTATIONAL_GRAPH_BASIC_CALC_PACK_H
