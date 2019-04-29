60998c3dbbf772484513a007783b574782346d7f
GodofTheFallen <gtf_2000@qq.com>
2019-04-29 22:28:43 +0800

    更新ConNode的注释

--- lib/connode.h
+++ lib/connode.h
@@ -9,11 +9,11 @@ class ConNode : public Node<_T>
 private:
     _T *const ConVal; //指向常量的指证，用来储存初始化的答案，不可更改
 public:
-    explicit ConNode(_T _InitData) : ConVal(new const _T(_InitData)) {};
+    explicit ConNode(const _T &_InitData) : ConVal(new const _T(_InitData)) {}; //从常量构造节点
 
-    _T GetVal();
+    _T GetVal(); //求值
 
-    void Clear();
+    void Clear(); //清除（虽然能被调用，但实际上什么都不做）
 
     ~ConNode()
     {
@@ -23,12 +23,12 @@ public:
 };
 
 template<typename _T>
-_T ConNode<_T>::GetVal()
+_T ConNode<_T>::GetVal() //直接返回常值
 {
     return *ConVal;
 }
 
 template<typename _T>
-void ConNode<_T>::Clear() {}
+void ConNode<_T>::Clear() {} //清除时什么都不做
 
 #endif //COMPUTATIONAL_GRAPH_CONNODE_H
