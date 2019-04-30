31b2fd5e8dcf80ee55b230bd20d9aabe92efb8c2
GodofTheFallen <gtf_2000@qq.com>
2019-04-30 09:32:20 +0800

    创建CondNode

--- compare_calc_pack/compare_calc_pack.h
+++ compare_calc_pack/compare_calc_pack.h
@@ -1,9 +1,11 @@
 #ifndef COMPUTATIONAL_GRAPH_COMPARE_CALC_PACK_H
 #define COMPUTATIONAL_GRAPH_COMPARE_CALC_PACK_H
+
 #include "gtcnode.h"
 #include "lscnode.h"
 #include "gecnode.h"
 #include "lecnode.h"
 #include "eqcnode.h"
+#include "condnode.h"
 
 #endif //COMPUTATIONAL_GRAPH_COMPARE_CALC_PACK_H
